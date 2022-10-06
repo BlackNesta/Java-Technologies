cc6cbff8589d7c105361eaf7a277a9cb3566b5eb
Andrei Manaila <andrei.manaila@takeofflabs.com>
2022-10-06 08:13:22 +0300

    Hello World

---
+++ HelloWorld/src/main/java/ro/uaic/info/hello/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package ro.uaic.info.hello;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
